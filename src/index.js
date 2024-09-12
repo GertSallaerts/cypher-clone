437d9eb1143863a50721f7a7bac5e10451591f4b
Jorgen Evens <jorgen@evens.eu>
2024-09-12 11:51:46 +0200

    feat(compat): add support for neo4j 5

--- src/index.js
+++ src/index.js
@@ -29,14 +29,44 @@ function getDriver(connectionString) {
     return neo4j.driver(connectionString, neo4j.auth.basic(user, password));
 }
 
+async function getSession(driver) {
+    const { agent } = await driver.getServerInfo();
+
+    const [ major = 0, minor = 0, patch = 0 ] = /Neo4j\/(\d+).(\d+).(\d+)/
+        .exec(agent)
+        .slice(1)
+        .map(v => parseInt(v, 10) || 0);
+
+    const session = driver.session();
+    session.serverVersion = { major, minor, patch };
+
+    return session;
+}
+
+function getSyntax(session) {
+    const is5 = session.serverVersion.major >= 5;
+
+    const constraints = is5 ? 'SHOW CONSTRAINTS YIELD *' : 'CALL db.constraints()';
+    const indexes = is5 ? 'SHOW INDEXES YIELD *' : 'CALL db.indexes()';
+
+    return {
+        is5,
+
+        constraints,
+        indexes,
+    };
+}
+
 async function dropIndexes(session) {
-    const constraints = await session.run('CALL db.constraints()');
+    const syntax = getSyntax(session);
+
+    const constraints = await session.run(syntax.constraints);
 
     await writeTransaction(session, constraints.records.map(r => ({
         query: `DROP CONSTRAINT ${r.get('name')}`
     })));
 
-    const indexes = await session.run('CALL db.indexes()');
+    const indexes = await session.run(syntax.indexes);
 
     await writeTransaction(session, indexes.records.map(r => ({
         query: `DROP INDEX ${r.get('name')}`
@@ -44,12 +74,21 @@ async function dropIndexes(session) {
 }
 
 async function copyIndexes(from, to) {
-    const indexes = await from.run('CALL db.indexes()');
-    const constraints = await from.run('CALL db.constraints()');
+    const fromSyntax = getSyntax(from);
+
+    const indexes = await from.run(fromSyntax.indexes);
+    const constraints = await from.run(fromSyntax.constraints);
     const indexProviders = {};
     const queries = [];
 
     indexes.records.forEach(r => {
+        if (fromSyntax.is5) {
+            const owningConstraint = r.get('owningConstraint');
+            if (!constraints.records.some(c => c.get('name') == owningConstraint))
+                queries.push({ query: r.get('createStatement') });
+            return;
+        }
+
         const name = r.get('name');
         const uniqueness = r.get('uniqueness');
         const type = r.get('type');
@@ -112,6 +151,11 @@ async function copyIndexes(from, to) {
 
 
     constraints.records.forEach(r => {
+        if (fromSyntax.is5) {
+            queries.push({ query: r.get('createStatement') });
+            return;
+        }
+
         const name = r.get('name');
         const query = [ `CREATE ${r.get('description')}` ];
 
@@ -156,12 +200,13 @@ async function writeTransaction(session, queries) {
 
 async function doSync(fromSession, toSession) {
     const startTime = Date.now();
+    const toSyntax = getSyntax(toSession);
 
     await paginateQuery(toSession, EMPTY_DB, 20000);
     await dropIndexes(toSession);
     console.log('Destination database emptied');
 
-    await toSession.run(CREATE_UNIQUE);
+    await toSession.run(CREATE_UNIQUE(toSyntax));
     console.log('Prepared UNIQUE constraint for sync performance');
 
     const writeQueue = new QueryBatcher(batch => writeTransaction(toSession, batch));
@@ -186,7 +231,7 @@ async function doSync(fromSession, toSession) {
     await paginateQuery(toSession, CLEANUP_UNIQUE, 20000);
     console.log('Removed temporary performance helpers');
 
-    await toSession.run(DROP_UNIQUE);
+    await toSession.run(DROP_UNIQUE(toSyntax));
     console.log('Removed performance helper UNIQUE contraint');
 
     await copyIndexes(fromSession, toSession);
@@ -196,11 +241,18 @@ async function doSync(fromSession, toSession) {
     console.log(`Sync completed in ${time} seconds`);
 }
 
-module.exports = function sync(from, to) {
+module.exports = async function sync(from, to) {
     const fromDriver = getDriver(from);
     const toDriver = getDriver(to);
-    const fromSession = fromDriver.session();
-    const toSession = toDriver.session();
+
+    const [ fromSession, toSession ] = await Promise.all([
+        getSession(fromDriver),
+        getSession(toDriver),
+    ]);
+
+    if (fromSession.serverVersion.major != toSession.serverVersion.major) {
+        throw new Error('Target and source database must be same major version.');
+    }
 
     const promise = doSync(fromSession, toSession);
 
@@ -211,5 +263,5 @@ module.exports = function sync(from, to) {
         toDriver.close();
     });
 
-    return promise;
+    return await promise;
 };
